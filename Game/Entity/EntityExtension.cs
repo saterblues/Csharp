2c636e6d9a56043e94aa57f48303f2463f7cf9aa
saterblues <80666621@qq.com>
2022-07-30 13:59:07 +0800

    Add files via upload

---
+++ Game/Entity/EntityExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.System;
+
+namespace Game
+{
+    public static partial class EntityExtension
+    {
+        public static bool IsInSystem<T>(this Entity entity) where T : ISystem
+        {
+            if (false == World.World.GameWorld.IsContainSystem<T>())
+            {
+                return false;
+            }
+            T t = World.World.GameWorld.GetSystem<T>();
+            return t.ContainsEntity(entity);
+        }
+
+        public static T GetSystem<T>(this Entity entity) where T : ISystem {
+            return World.World.GameWorld.GetSystem<T>();
+        }
+    }
+}
---
+++ Game/System/ISystem.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.System
+{
+    /// <summary>
+    /// 系统基类
+    /// 管理所有注册的Entity,生成对应的组件并放入数组
+    /// </summary>
+    public abstract class ISystem{
+        public struct RegistInfo {
+            public Entity entity;
+            public object[] args;
+
+            public RegistInfo(Entity entity, object[] args)
+            {
+                this.entity = entity;
+                this.args = args;
+            }
+        }
+
+        public event EventHandler<RegistInfo> OnRegist = (sender, registInfo) => { };
+        public event EventHandler<Entity> OnUnRegist = (sender, entity) => { };
+
+        protected Dictionary<Entity, IComponent> _components = new Dictionary<Entity, IComponent>();
+
+        protected virtual void IfNotFoundThrowException(Entity entity)
+        {
+            if (false == ContainsEntity(entity))
+            {
+                string msg = string.Format("Entity Hash:{0} Uid:{1} Not In System:{2}",
+                    entity.GetHashCode(),
+                    entity.Uid,
+                    GetType().FullName);
+
+                throw new KeyNotFoundException(msg);
+            }
+        }
+
+        public virtual IComponent GetComponent(Entity entity)
+        {
+            IComponent com;
+            _components.TryGetValue(entity, out com);
+            return com;
+        }
+
+        public virtual T GetComponent<T>(Entity entity) where T : IComponent
+        {
+            if (GetComponent(entity) == null) { return null; }
+            return GetComponent(entity) as T;
+        }
+
+        public virtual IEnumerable<Entity> GetEntities() {
+            return _components.Keys;
+        }
+
+        public virtual void RegistEntity(Entity entity, params object[] args) {
+            IComponent com = CreateComponent(entity, args);
+            _components.Add(entity, com);
+
+            OnRegist.Invoke(this, new RegistInfo(entity, args));
+        }
+
+        public virtual void UnRegistEntity(Entity entity)
+        {
+            _components.Remove(entity);
+
+            OnUnRegist.Invoke(this, entity);
+        }
+  
+        public virtual bool ContainsEntity(Entity entity) {
+            return _components.ContainsKey(entity);
+        }
+
+        public virtual void Update() { }
+
+        public virtual void SetData(Entity entity, IComponent component){ }
+
+        public abstract IComponent CreateComponent(Entity entity,params object[] args);
+    }
+}
