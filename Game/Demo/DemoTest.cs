12306b3aaa7c26ea1cf2eebcc13c99ade3cba748
saterblues <80666621@qq.com>
2022-07-31 22:16:55 +0800

    Add files via upload

---
+++ Game/Demo/DemoTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.System.Data;
+using Game.System.Logic;
+using Game.Component;
+
+namespace Game.Demo
+{
+    public class DemoTest
+    {
+        public void Test() {
+            World.World world = World.World.GameWorld;
+            world.RegistSystem<NameSystem>();
+            world.RegistSystem<HealthSystem>();
+            world.RegistSystem<ManaSystem>();
+            world.RegistSystem<IntelligenceSystem>();
+            world.RegistSystem<StrengthSystem>();
+            world.RegistSystem<AgilitySystem>();
+            world.RegistSystem<PowerSystem>();
+            world.RegistSystem<UISystem>();
+            world.UnRegistSystem<ManaSystem>();
+
+
+            Entity e1 = world.CreateEntity();
+            Entity e2 = world.CreateEntity();
+
+            e1.RegistToSystem<NameSystem>("壹号玩家");
+            e2.RegistToSystem<NameSystem>("二号玩家");
+            e1.RegistToSystem<PowerSystem>(10, 1, 5);
+
+            world.Update();
+
+            e1.GetSystem<PowerSystem>().SetAgility(e1, 888);
+            world.Update();
+
+            e2.Destroy();
+            world.Update();
+
+            var x1 = e1.GetSystem<AgilitySystem>().GetComponent<SingleComponent>(e1).value;
+            Console.WriteLine(x1);
+        }
+    }
+}
---
+++ Game/System/Logic/UISystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Game.System.Data;
+using Game.Component;
+
+namespace Game.System.Logic
+{
+    /// <summary>
+    /// UI系统
+    /// 负责显示UI
+    /// </summary>
+    public class UISystem : ISystem
+    {
+        public override IComponent CreateComponent(Entity entity, params object[] args)
+        {
+            return new BaseComponent();
+        }
+
+        public override void Update()
+        {
+           var entities = World.World.GameWorld.GetEntites();
+           foreach (var item in entities)
+           {
+               string name = "";
+               
+               if (item.IsInSystem<NameSystem>()) {
+                   name = item.GetSystem<NameSystem>().GetName(item);
+               }
+               string power = "";
+               if (item.IsInSystem<PowerSystem>()) {
+                   var powerSystem = item.GetSystem<PowerSystem>();
+                   power = string.Format("智力:{0} 力量:{1} 敏捷:{2}",
+                           powerSystem.GetIntelligence(item),
+                           powerSystem.GetStrength(item),
+                           powerSystem.GetAgility(item)
+                       );
+               }
+
+               Console.WriteLine("玩家:{0} {1}",name,power);
+            }
+        }
+    }
+}
